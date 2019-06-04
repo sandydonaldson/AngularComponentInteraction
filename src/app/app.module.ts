d9312b6d00818ba86f550a1d1a24484027210e68
sandydonaldson <git@sandydonaldson.com>
2019-06-04 11:09:31 +0100

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child/child.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ChildComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
