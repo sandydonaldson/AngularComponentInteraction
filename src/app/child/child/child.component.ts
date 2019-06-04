9f361adf0679b78eaefe94f13ba6b4393e0dfae3
sandydonaldson <git@sandydonaldson.com>
2019-06-04 11:40:21 +0100

    1 file modified

--- src/app/child/child/child.component.ts
+++ src/app/child/child/child.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnChanges {
 
-  private _loggedIn : boolean;
+  @Input() loggedIn : boolean;
   message : string;
 
+/*
   get loggedIn(): boolean {
     return this._loggedIn;
   }
@@ -22,10 +23,18 @@ export class ChildComponent implements OnInit {
       this.message = 'Please log in'       
     }
   }
-
+*/
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes:SimpleChanges) {
+    console.log(changes);
+
+    const loggedInValue = changes['loggedIn'];
+    if (loggedInValue.currentValue === true){
+      this.message = 'Welcome back';
+    } else {
+      this.message = 'Please log in';
+    }
   }
 
 }
