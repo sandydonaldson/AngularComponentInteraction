79d783840e482ff94380f4fefdf54ca525263745
sandydonaldson <git@sandydonaldson.com>
2019-06-05 10:53:36 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { ChildComponent } from './child/child/child.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements AfterViewInit  {
   userLoggedIn = true;
+  @ViewChild(ChildComponent) childComponentRef: ChildComponent;
+
+  ngAfterViewInit(){
+    this.childComponentRef.message = 'Message from Parent Component';
+  }
 }
